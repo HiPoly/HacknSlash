618f3d1f4205cf80f6380c7136fa7a373e94a7cc
Al <theonlypurplecat@gmail.com>
2020-11-02 13:40:58 +1300

    Menu Basics

---
+++ Assets/Scripts/MenuScripts/MainMenu.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    //Level Variables
+    [SerializeField]
+    private string FirstLevel;
+
+
+
+
+    // in menu pages/changes:
+
+    [SerializeField]
+    private GameObject SettingsMenu;
+
+    [SerializeField]
+    private GameObject QuitPrompt;
+
+
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    //Start the Game things:
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(FirstLevel);
+    }
+
+
+
+
+    //Settings Menus:
+
+    //turn Settings menu on
+    public void SettingsOn()
+    {
+        SettingsMenu.SetActive(true);
+    }
+
+    //turn settings menu off
+    public void SettingsOff()
+    {
+        SettingsMenu.SetActive(false);
+    }
+
+
+
+
+
+
+    //Quit things:
+
+    //when selecting the quit button bring up the Quit Yes/no option
+    public void QuitPromptOn()
+    {
+        QuitPrompt.SetActive(true);
+    }
+
+    //selecting No and closing the quit prompt
+    public void QuitPromptOff()
+    {
+        QuitPrompt.SetActive(false);
+    }
+    
+    //selecting yes and quitting the game
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Assets/Scripts/MenuScripts/PauseMenu.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    //pause menu direct things
+    [SerializeField]
+    private GameObject thePauseMenu;
+    [SerializeField]
+    private KeyCode PauseTheGame;
+
+    private bool GameIsPaused;
+
+
+    //scene Loading variables
+
+    [SerializeField]
+    private string TitleScene;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(PauseTheGame))
+        {
+
+            if (GameIsPaused)
+            {
+                PauseMenuOff();
+            }
+            else
+            {
+                PauseMenuOn();
+            }
+
+
+        }
+
+    }
+
+    //turn pause menu on/pause the game
+    private void PauseMenuOn()
+    {
+
+        Time.timeScale = 0;
+        thePauseMenu.SetActive(true);
+        GameIsPaused = true;
+
+    }
+
+
+    // turn the pause menu off/Un-pause the game
+    private void PauseMenuOff()
+    {
+        Time.timeScale = 1;
+        thePauseMenu.SetActive(false);
+        GameIsPaused = false;
+
+    }
+
+
+
+    //Menu Button behaviours
+    public void ResumeGame()
+    {
+        PauseMenuOff();
+    }
+
+
+
+
+
+    public void TitleScreen()
+    {
+
+        SceneManager.LoadScene(TitleScene);
+
+    }
+
+    
+
+
+
+
+
+
+
+
+}
